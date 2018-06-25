4a0e67ec030b3889efc9826ded7cd4cb706c3858
Melissa S Smith <melissasmith358@gmail.com>
2018-06-24 18:18:51 -0700

    Add hex values for srm and inputs to calculate srm and ebc

--- src/App.fs
+++ src/App.fs
@@ -3,17 +3,55 @@ module SrmCalculator
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Import.Browser
+open SrmCalculations
 
-let init() =
+type IJQuery = 
+    [<Emit("$0.click($1)")>]
+    abstract OnClick : (obj -> unit) -> IJQuery
+
+module JQuery =
+
+  [<Emit("window['$']($0)")>]
+  let ready (handler: unit -> unit) : unit = jsNative
+  
+  [<Emit("$2.css($0, $1)")>]
+  let css (prop: string) (value: string) (el: IJQuery) : IJQuery = jsNative
+  
+  [<Emit("$1.click($0)")>]
+  let click (handler: obj -> unit)  (el: IJQuery) : IJQuery = jsNative
+
+  [<Emit("window['$']($0)")>]
+  let select (selector: string) : IJQuery = jsNative
+
+let calculateSrm batchSize amount degreesLovibond = 
+    SrmColor degreesLovibond (TransformToDecimal amount) (TransformToDecimal batchSize)
+    |> RoundToPrecisonTwo
+    |> TransformToString
+
+let calculateEbc batchSize amount degreesLovibond = 
+    Ebc degreesLovibond (TransformToDecimal amount) (TransformToDecimal batchSize)
+    |> RoundToPrecisonTwo
+    |> TransformToString
+
+let fillColor (hexColor: string) = 
     let canvas = Browser.document.getElementsByTagName_canvas().[0]
     canvas.width <- 1000.
-    canvas.height <- 800.
+    canvas.height <- 1000.
     let ctx = canvas.getContext_2d()
-    // The (!^) operator checks and casts a value to an Erased Union type
-    // See http://fable.io/docs/interacting.html#Erase-attribute
-    ctx.fillStyle <- !^"rgb(200,0,0)"
+    ctx.fillStyle <- !^hexColor
     ctx.fillRect (10., 10., 55., 50.)
-    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
-    ctx.fillRect (30., 30., 55., 50.)
 
-init()
+let mainLoop ev =
+    let batchSize = document.getElementById("batchSize")?value
+    let amount1 = document.getElementById("amount1")?value
+    let degreesLovibond = 17.0m
+    let srm = calculateSrm batchSize amount1 degreesLovibond
+    let ebc = calculateEbc batchSize amount1 degreesLovibond
+    document.getElementById("srmResult")?innerHTML <- srm
+    document.getElementById("ebcResult")?innerHTML <- ebc
+    |> ignore
+
+JQuery.select("#calculate")
+    .OnClick(mainLoop)
+    |> ignore
--- src/SrmCalculator.fs
+++ src/SrmCalculator.fs
@@ -2,33 +2,44 @@ module SrmCalculations
 
 open Fable.Core
 open System
+open System.Xml.Linq
 
 [<Emit("isNaN(parseFloat($0)) ? null : parseFloat($0)  ")>]
 let ParseFloat (e : obj) : float option = jsNative
+
+let TransformToString r = r |> string
 
 let TransformToDecimal e = 
     match ParseFloat e with
     | Some value -> value |> decimal
     | None -> 0.0m
 
-let TransformToDouble e : double = 
-    Decimal.ToDouble e
+let RoundDecimal (input : decimal) (precision: int) = 
+    Math.Round(input, precision)
 
-let DecimalPower (x: decimal) (y:decimal) =
-    Math.Pow ((TransformToDouble x), (TransformToDouble y))
+let RoundToPrecisonTwo x =
+    RoundDecimal x 2
+
+let TransformToDouble e : double = 
+    match ParseFloat e with
+    | Some value -> value |> double
+    | None -> 0.0
+
+let DecimalPower (power: decimal) (b:decimal) =
+    Math.Pow ((TransformToDouble b), (TransformToDouble power))
     |> TransformToDecimal
 
 let MultiplyByConstant variable constant = 
     variable * constant
 
-let Mcu grainColor grainWeightLbs VolGal : decimal =
-    (grainColor * grainWeightLbs) / VolGal
+let Mcu grainColor grainWeightLbs volGal : decimal =
+    (grainColor * grainWeightLbs) / volGal
 
-let SrmColor grainColor grainWeightLbs VolGal =
-    Mcu grainColor grainWeightLbs VolGal
+let SrmColor grainColor grainWeightLbs volGal =
+    Mcu grainColor grainWeightLbs volGal
     |> DecimalPower 0.6859m
     |> MultiplyByConstant 1.4922m
 
-let Ebc grainColor grainWeightLbs VolGal = 
-    SrmColor grainColor grainWeightLbs VolGal
+let Ebc grainColor grainWeightLbs volGal = 
+    SrmColor grainColor grainWeightLbs volGal
     |> MultiplyByConstant 1.97m
