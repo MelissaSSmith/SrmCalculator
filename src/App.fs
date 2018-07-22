410a6b0623d89e6e68d1111026c2956a75ca2515
Melissa S Smith <melissasmith358@gmail.com>
2018-07-22 11:49:37 -0700

    Set srm color based on input

--- src/App.fs
+++ src/App.fs
@@ -43,6 +43,10 @@ type Fermentable =
       DegreesLovibond: int
       Ppg: int }
 
+type SrmHex =
+    { SrmKey: int
+      HexValue: string}
+
 let calculateSrm batchSize amount degreesLovibond = 
     SrmColor (TransformToFloat degreesLovibond) (TransformToFloat amount) (TransformToFloat batchSize)
     |> RoundToPrecisonTwo
@@ -63,18 +67,28 @@ let fillColor (hexColor: string) =
 
 [<Literal>]
 let FermentableFile = "file:\\\\C:\\Users\\melissaSusan\\source\\repos\\SrmCalculator\\src\\Fermentables.json" 
+[<Literal>]
+let SrmHexFile = "file:\\\\C:\\Users\\melissaSusan\\source\\repos\\SrmCalculator\\src\\SrmHex.json" 
 
 let Fermentables = @"[ { ""Id"": 1,""Name"": ""Abbey Malt"",""Country"": ""German"",""Category"": ""Grain"",""Type"": ""Base Malt"",""DegreesLovibond"": 17,""Ppg"": 33  }]"
-
+let SrmHexValues = @"[{""SrmKey"":1,""HexValue"":""#FFE699""},{""SrmKey"":2,""HexValue"":""#FFD878""},{""SrmKey"":3,""HexValue"":""#FFCA5A""},{""SrmKey"":4,""HexValue"":""#FFBF42""},{""SrmKey"":5,""HexValue"":""#FBB123""},{""SrmKey"":6,""HexValue"":""#F8A600""},{""SrmKey"":7,""HexValue"":""#F39C00""},{""SrmKey"":8,""HexValue"":""#EA8F00""},{""SrmKey"":9,""HexValue"":""#E58500""},{""SrmKey"":10,""HexValue"":""#DE7C00""},{""SrmKey"":11,""HexValue"":""#D77200""},{""SrmKey"":12,""HexValue"":""#CF6900""},{""SrmKey"":13,""HexValue"":""#CB6200""},{""SrmKey"":14,""HexValue"":""#C35900""},{""SrmKey"":15,""HexValue"":""#BB5100""},{""SrmKey"":16,""HexValue"":""#B54C00""},{""SrmKey"":17,""HexValue"":""#B04500""},{""SrmKey"":18,""HexValue"":""#A63E00""},{""SrmKey"":19,""HexValue"":""#A13700""},{""SrmKey"":20,""HexValue"":""#9B3200""},{""SrmKey"":21,""HexValue"":""#952D00""},{""SrmKey"":22,""HexValue"":""#8E2900""},{""SrmKey"":23,""HexValue"":""#882300""},{""SrmKey"":24,""HexValue"":""#821E00""},{""SrmKey"":25,""HexValue"":""#7B1A00""},{""SrmKey"":26,""HexValue"":""#771900""},{""SrmKey"":27,""HexValue"":""#701400""},{""SrmKey"":28,""HexValue"":""#6A0E00""},{""SrmKey"":29,""HexValue"":""#660D00""},{""SrmKey"":30,""HexValue"":""#5E0B00""},{""SrmKey"":31,""HexValue"":""#5A0A02""},{""SrmKey"":32,""HexValue"":""#600903""},{""SrmKey"":33,""HexValue"":""#520907""},{""SrmKey"":34,""HexValue"":""#4C0505""},{""SrmKey"":35,""HexValue"":""#470606""},{""SrmKey"":36,""HexValue"":""#440607""},{""SrmKey"":37,""HexValue"":""#3F0708""},{""SrmKey"":38,""HexValue"":""#3B0607""},{""SrmKey"":39,""HexValue"":""#3A070B""},{""SrmKey"":40,""HexValue"":""#36080A""}]"
 let getFermentables =
     ofJson<Fermentable list> Fermentables
     |> Seq.sortBy(fun f -> f.Category)
     |> Seq.sortBy(fun f -> f.Country)
     |> Seq.sortBy(fun f -> f.Name)
 
+let getSrmHexList =
+    ofJson<SrmHex list> SrmHexValues
+
 let getDegreesLovibondForFermentable id =
     let ferm = Seq.find(fun f -> f.Id = id) getFermentables
     ferm.DegreesLovibond
+
+let getSrmHexValue srm =
+    let hex = Seq.find(fun f -> f.SrmKey = (TransformToInt srm)) getSrmHexList
+    hex.HexValue
+
 
 let FillInFermentables dropdownId (element: IJQuery) =
     for f in getFermentables do
@@ -106,11 +120,16 @@ let mainLoop ev =
     let amount1 = document.getElementById("amount1")?value
     let grain1 = TransformToInt(document.getElementById("grain1")?value)
     let degreesLovibond = getDegreesLovibondForFermentable grain1
-    calculateSrm batchSize amount1 degreesLovibond
+    let srm = calculateSrm batchSize amount1 degreesLovibond
+    srm
     |> setResult "srmResult"
     |> ignore
     calculateEbc batchSize amount1 degreesLovibond
     |> setResult "ebcResult"
+    |> ignore
+    let color = getSrmHexValue srm
+    JQuery.select("#approxColor")
+    |> JQuery.css "background-color" color
     |> ignore
 
 JQuery.select("#calculate")
